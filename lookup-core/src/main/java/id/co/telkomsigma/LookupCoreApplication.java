c5c5ef4bd3eae0a86149a7b513f8a23d0bb50992
Sarasuartha <sarasuartha@sigma.co.id>
2015-05-19 10:56:35 +0800

    Create Lookup Core

---
+++ lookup-core/src/main/java/id/co/telkomsigma/LookupCoreApplication.java
@@ -0,0 +1,12 @@
+package id.co.telkomsigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookupCoreApplication.class, args);
+    }
+}
