ebfcac323aa7885bb888f8ab81e3f24e53ddd63d
Agam <agampradhana@gmail.com>
2015-05-19 10:49:32 +0800

    create new project lookup-gui

---
+++ lookup-gui/src/main/java/id/co/telkomsigma/lookup/gui/LookupGuiApplication.java
@@ -0,0 +1,12 @@
+package id.co.telkomsigma.lookup.gui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupGuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookupGuiApplication.class, args);
+    }
+}
